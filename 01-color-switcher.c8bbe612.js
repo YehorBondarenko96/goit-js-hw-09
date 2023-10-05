82905c4a9778e1cd8efab38cd5f4c4d56e4db04e
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2023-10-05 17:08:00 +0000

    Deploying to gh-pages from @ YehorBondarenko96/goit-js-hw-09@0393ebcc597bbbb231ab22edfa6cdef574b73d7c 🚀

---
+++ 01-color-switcher.c8bbe612.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");t.addEventListener("click",(()=>{t.disabled=!0,a.disabled=!1,repeater=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),a.addEventListener("click",(()=>{t.disabled=!1,a.disabled=!0,clearInterval(repeater)}));
+//# sourceMappingURL=01-color-switcher.c8bbe612.js.map
