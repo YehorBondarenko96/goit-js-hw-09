69edbf28149cbf463c259d0368116bee20beef6e
YehorBondarenko96 <egorbondarenko1996@gmail.com>
2023-10-05 20:49:04 +0300

    1.2-05.10.23

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,7 @@
     const body = document.querySelector('body');
     const start = document.querySelector('button[data-start]');
     const stop = document.querySelector('button[data-stop]');
-
+    let repeater;
     function getRandomHexColor() {
         return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
     };
