0393ebcc597bbbb231ab22edfa6cdef574b73d7c
YehorBondarenko96 <egorbondarenko1996@gmail.com>
2023-10-05 20:07:30 +0300

    1-05.10.23

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+    const body = document.querySelector('body');
+    const start = document.querySelector('button[data-start]');
+    const stop = document.querySelector('button[data-stop]');
 
+    function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    };
+
+start.addEventListener('click', () => {
+    start.disabled = true;
+    stop.disabled = false;
+    repeater = setInterval(() => { body.style.backgroundColor = getRandomHexColor() }, 1000)
+});
+
+stop.addEventListener('click', () => {
+    start.disabled = false;
+    stop.disabled = true;
+   clearInterval(repeater) 
+});
