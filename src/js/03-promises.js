89ecf577267e6ce8b4c5e25ed289102ea1336ff6
YehorBondarenko96 <egorbondarenko1996@gmail.com>
2023-10-07 00:18:00 +0300

    3.1-07/10/23

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,43 @@
+import Notiflix from 'notiflix';
+
 const firstDelay = document.querySelector('input[name="delay"]');
 const delayStep = document.querySelector('input[name="step"]');
 const amount = document.querySelector('input[name="amount"]');
-const button = document.querySelector('button');
+const form = document.querySelector('.form');
+
+
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    const idInterval = setInterval(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+    } else {
+        reject({position, delay});
+    }
+    }, delay);
+  })
+};
+
+function multiplyPromises(event) {
+  event.preventDefault();
+  let delay = Number(firstDelay.value);
+  for (let position = 1; position <= Number(amount.value); position++) {
+    console.log(delay);
+    const promis = createPromise(position, delay);
+    promis.then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }).catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    })
+    
+    delay += Number(delayStep.value);
   }
-}
+};
+
+form.addEventListener('submit', multiplyPromises);
+
+
+
